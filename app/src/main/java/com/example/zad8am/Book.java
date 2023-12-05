6bd5a72dc98e1292cf1480a0e681628a7f8ea238
Łukasz Leończak <82775@student.pb.edu.pl>
2023-12-05 09:18:50 +0100

    zad8 (nie gotowe 16/26)

---
+++ app/src/main/java/com/example/zad8am/Book.java
@@ -0,0 +1,47 @@
+package com.example.zad8am;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Entity(tableName = "book")
+public class Book {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String author;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ app/src/main/java/com/example/zad8am/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.zad8am;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.zad8am.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private BookViewModel bookViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final BookAdapter adapter = new BookAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        bookViewModel = new ViewModelProvider(this).get(BookViewModel.class);
+        bookViewModel.findAll().observe(this,adapter::setBooks);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private class BookHolder extends RecyclerView.ViewHolder{
+        private TextView bookTitleTextView;
+        private TextView bookAuthorTextView;
+
+        public BookHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.book_list_item,parent,false));
+
+            bookTitleTextView = itemView.findViewById(R.id.book_title);
+            bookAuthorTextView = itemView.findViewById(R.id.bool_author);
+        }
+        public void bind(Book book){
+            bookTitleTextView.setText(book.getTitle());
+            bookAuthorTextView.setText(book.getAuthor());
+        }
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder> {
+        private List<Book> books;
+
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+            return new BookHolder(getLayoutInflater(),parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position){
+            if(books != null){
+                Book book = books.get(position);
+                holder.bind(book);
+            }else{
+                Log.d("MainActivity","No books");
+            }
+        }
+
+        @Override
+        public int getItemCount(){
+            if(books != null){
+                return books.size();
+            }else{
+                return 0;
+            }
+        }
+
+        void setBooks(List<Book> books){
+            this.books = books;
+            notifyDataSetChanged();
+        }
+
+    }
+
+
+}
